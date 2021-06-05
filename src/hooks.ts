baa913e6a0f633e49d97963df4643577ab248eb3
Anwesh43 <anweshthecool0@gmail.com>
2021-06-05 17:12:22 +0530

    adding useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, CSSProperties} from 'react'
 
 const scGap : number = 0.02 
 const delay : number = 20 
@@ -36,12 +36,53 @@ export const useDimension = () => {
         }
         return () => {
             window.onresize = () => {
-                
+
             }
         }
     })
     return {
         w, 
         h
+    }
+}
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const position = 'absolute'
+    const size : number = Math.min(w, h) / 10
+    const sf : number = Math.sin(scale * Math.PI)
+    const fixedX : number = w / 2 
+    const fixedY : number = h / 2
+    const x : number = fixedX + (w / 2 - size / 2) * (1 - sf)
+    const y : number = fixedY - (h / 2 - size / 2) * sf 
+    const width = `${size}px`
+    const height = `${size}px`
+    const background = '#00C853'
+    return {
+        blockStyle() : CSSProperties {
+            const left = `${x - size / 2}px`
+            const top = `${fixedY - size / 2}px`
+            return {
+                left, 
+                top, 
+                position, 
+                width, 
+                height,
+                background  
+            }
+        },
+        circleStyle() : CSSProperties {
+            const left = `${fixedX - size / 2}px`
+            const top = `${y - size / 2}px`
+            const borderRadius = '50%'
+            return {
+                position, 
+                left, 
+                top, 
+                borderRadius, 
+                width, 
+                height,
+                background 
+            }
+        }
     }
 }
